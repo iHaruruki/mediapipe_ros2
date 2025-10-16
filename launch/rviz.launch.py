0acbeb9426790b69adc9b043f692d0ee7a15e7f5
robot <ryo.saegusa@syblab.org>
2025-10-16 17:03:34 +0900

    fix

---
+++ launch/rviz.launch.py
@@ -0,0 +1,21 @@
+from ament_index_python import get_package_share_directory
+from launch import LaunchDescription
+from launch_ros.actions import Node
+import os
+
+def generate_launch_description():
+    
+    rviz_config_dir = os.path.join(
+        get_package_share_directory('mediapipe_ros2'),
+        'rviz', 'rviz2.rviz'
+    )
+
+    return LaunchDescription([
+        Node(
+            package='rviz2',
+            executable='rviz2',
+            name='rviz2',
+            arguments=['-d', rviz_config_dir],
+            output='screen'
+        ),
+    ])
