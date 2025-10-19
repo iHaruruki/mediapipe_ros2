e6ea20da4babb71d4840511870cb37bd758853cf
HarukiIsono <haruki.isono861@gmail.com>
2025-10-19 21:03:19 +0900

    fix

--- mediapipe_ros2/sub_landmarks.py
+++ mediapipe_ros2/sub_landmarks.py
@@ -3,19 +3,20 @@ from rclpy.node import Node
 from std_msgs.msg import Float32MultiArray
 
 class LandmarkSubscriber(Node):
-    def __init__('landmark_subscriber')
-    self.subscription = self.create_subscription(
-        Float32MultiArray,
-        'landmarks',
-        self.subscriber_callback,
-        10)
-    self.subscription  # prevent unused variable warning
+    def __init__(self):
+        super().__init__('landmark_subscriber')
+        self.subscription = self.create_subscription(
+            Float32MultiArray,
+            'landmarks',
+            self.subscriber_callback,
+            10)
+        self.subscription  # prevent unused variable warning
 
-    def subscriber_callback(self, msg):
-        num = len(msg.data)
-        self.get_logger().info('Message length: %d' % num)
-        for i in range(num):
-            self.get_logger().info('Landmark %d: %f' % (i, msg.data[i]))
+        def subscriber_callback(self, msg):
+            num = len(msg.data)
+            self.get_logger().info('Message length: %d' % num)
+            for i in range(num):
+                self.get_logger().info('Landmark %d: %f' % (i, msg.data[i]))
 
 def main(args=None):
     rclpy.init(args=args)
